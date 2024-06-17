4a993507c3fc0c250ab57abf8ec9721bbf29b591
Leticia <leticiaevuy@gmail.com>
2024-06-17 13:29:07 +0100

    Add more datatypes

---
+++ data-types.ts
@@ -0,0 +1,44 @@
+const a="I am ";
+const b="happy ";
+const ab=a+b;
+const ab2=`${a} ${b}`;
+console.log(ab)
+console.log(ab2)
+
+// NUMBER
+// - INTEGER
+// - FLOAT
+
+const num =5;
+const num2 =9;
+
+const sum = num = num2;
+const product = num * num2;
+const quotient = num2 / num;
+const sqr = num ** 2; // num * num
+const pwr3 = num ** 3; // num * num *num
+const root2 = num2 ** (1/2); //square root of num2
+
+console.log("------------RESULTS-------------")
+console.log({
+    sum,
+    product,
+    quotient,
+    sqr,
+    pwr3,
+    root2
+})
+
+
+//ARRAY (an ITERABLE)
+const arr = [2, 2, 2, 2 "obinna", " Nigeria",[9, "ten", [78]]];
+const shoppinglistMen = ["boxers", "singlets", "data","ties","pants","shoes", "mummy's shoes"];
+const shoppinglistladies = ["bags", "ear rings", "shoes", "perfume", "make-up box"];
+
+
+
+
+
+// DICTIONARY (OBJECT)
+// key-value pairs
+//
